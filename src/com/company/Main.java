2c1bc4a8175dbc976efd230d759826ec743549d9
MiguelBenalcazar <mabhvebhmimm@gmail.com>
2019-10-17 18:35:39 +0800

    Week Day

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -54,9 +54,9 @@ public class Main {
                 break;
         }
 
-        String month = "December";
-        switch(month){
-            case "January":
+        String month = "January";
+        switch(month.toLowerCase()){
+            case "january":
                 System.out.println("Jan");
                 break;
             case "June":
@@ -64,6 +64,34 @@ public class Main {
                 break;
             default:
                 System.out.println("Not sure");
+        }
+
+        int weekDay = 15;
+        switch (weekDay){
+            case 0:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("ERROR");
+                break;
         }
 
     }
